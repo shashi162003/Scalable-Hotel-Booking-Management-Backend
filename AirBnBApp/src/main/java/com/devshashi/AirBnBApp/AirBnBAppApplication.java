872fd14f0140c1dd493f0c2bf5f29b82d0c6eafb
masterShashi <shashikumargupta443@gmail.com>
2026-02-20 09:51:14 +0530

    Project Init

---
+++ AirBnBApp/src/main/java/com/devshashi/AirBnBApp/AirBnBAppApplication.java
@@ -0,0 +1,13 @@
+package com.devshashi.AirBnBApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnBAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnBAppApplication.class, args);
+	}
+
+}
---
+++ AirBnBApp/src/test/java/com/devshashi/AirBnBApp/AirBnBAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devshashi.AirBnBApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnBAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
